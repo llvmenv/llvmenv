232d6f00225117b505ac3dd43730dbcb2f4215a0
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-10-25 01:32:30 +0900

    Fix module imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate toml;
-#[macro_use]
-extern crate log;
-#[macro_use]
-extern crate failure;
-extern crate dirs;
-extern crate glob;
-extern crate itertools;
-extern crate regex;
-extern crate reqwest;
-extern crate tempfile;
-
 pub mod build;
 pub mod config;
 pub mod entry;
