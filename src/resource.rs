2455cac8fb9d93cf4a46f4239fa16377fd5dccac
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-10-29 00:10:28 +0900

    Remove tmp dir before teset

--- src/resource.rs
+++ src/resource.rs
@@ -252,9 +252,12 @@ mod tests {
         let tar = Resource::Tar {
             url: "https://github.com/termoshtt/llvmenv/archive/0.1.10.tar.gz".into(),
         };
-        let tmp_dir = TempDir::new_in(cache_dir())?;
-        tar.download(tmp_dir.path())?;
-        let cargo_toml = tmp_dir.path().join("Cargo.toml");
+        let tmp_dir = cache_dir().join("_llvmenv_test");
+        if tmp_dir.exists() {
+            fs::remove_dir_all(&tmp_dir)?;
+        }
+        tar.download(&tmp_dir)?;
+        let cargo_toml = tmp_dir.join("Cargo.toml");
         assert!(cargo_toml.exists());
         Ok(())
     }
